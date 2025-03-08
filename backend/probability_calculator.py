56c4a6590509e1e6ec6007df37d7742ee95a4848
Filip746 <fkvesic@mathos.hr>
2025-03-09 00:59:54 +0100

    Integrated API service to fetch data

--- backend/probability_calculator.py
+++ backend/probability_calculator.py
@@ -3,6 +3,8 @@ import math
 import requests
 import csv
 from datetime import datetime
+from bs4 import BeautifulSoup
+import json
 
 def fetch_elo_ratings():
     today = datetime.today().date()
@@ -20,6 +22,100 @@ def fetch_elo_ratings():
         return teams
     else:
         print("Failed to fetch Elo ratings")
+        return None
+
+def get_fixtures(tournament_id, season_id):
+    team_name_map = {
+        "NK Istra 1961": "Istra 1961",
+        "HNK Gorica": "HNK Gorica",  
+        "NK Lokomotiva Zagreb": "Lok Zagreb",  
+        "GNK Dinamo Zagreb": "Dinamo Zagreb",  
+        'HNK Rijeka' : "Rijeka",
+        'HNK Hajduk Split' : "Hajduk",
+        'NK Osijek' : "Osijek",
+        'NK Varaždin' : "Varazdin",
+        'NK Slaven Belupo' : "Slaven Belupo",
+        'HNK Šibenik' : "Sibenik"
+    }
+
+    fixtures = []
+    for round_number in range(25, 37):  # Loop through rounds 1 to 36
+        url = f'https://www.sofascore.com/api/v1/unique-tournament/{tournament_id}/season/{season_id}/events/round/{round_number}'
+        headers = {
+            'accept': '*/*',
+            'accept-language': 'hr-HR,hr;q=0.5',
+            'cache-control': 'max-age=0',
+            'if-none-match': 'W/"18ce8527ac"',
+            'priority': 'u=1, i',
+            'referer': 'https://www.sofascore.com/tournament/football/croatia/hnl/170',
+            'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Brave";v="134"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'sec-gpc': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+            'x-requested-with': 'c728f9',
+        }
+        response = requests.get(url, headers=headers)
+        
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            for event in data.get('events', []): 
+                    home_team = event.get('homeTeam', {}).get('name')
+                    away_team = event.get('awayTeam', {}).get('name')
+                    if home_team and away_team:
+                        fixtures.append((home_team, away_team))
+        else:
+            print(f"Failed to fetch fixtures for round {round_number}")
+    
+    normalized_fixtures = []
+    for home_team, away_team in fixtures:
+        home_team = team_name_map.get(home_team, home_team)
+        away_team = team_name_map.get(away_team, away_team)
+        normalized_fixtures.append((home_team, away_team))
+    
+    return normalized_fixtures
+
+def get_standings(tournament_id, season_id):
+    url = f'https://www.sofascore.com/api/v1/unique-tournament/{tournament_id}/season/{season_id}/standings/total'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        standings = data.get('standings', [{}])[0].get('rows', [])
+        
+        team_name_map = {
+            "NK Istra 1961": "Istra 1961",
+            "HNK Gorica": "HNK Gorica",  
+            "NK Lokomotiva Zagreb": "Lok Zagreb",  
+            "GNK Dinamo Zagreb": "Dinamo Zagreb",  
+            'HNK Rijeka' : "Rijeka",
+            'HNK Hajduk Split' : "Hajduk",
+            'NK Osijek' : "Osijek",
+            'NK Varaždin' : "Varazdin",
+            'NK Slaven Belupo' : "Slaven Belupo",
+            'HNK Šibenik' : "Sibenik"
+        }
+        
+        initial_points = {}
+        for row in standings:
+            team_name = row.get('team', {}).get('name')
+            team_name = team_name_map.get(team_name, team_name)  # Normalize team name
+            
+            # Directly access points from the row
+            points = row.get('points', 0)
+            
+            initial_points[team_name] = points
+        
+        return initial_points
+    else:
+        print("Failed to fetch standings")
         return None
 
 def simulate_match(home_team_elo, away_team_elo, home_advantage=50, k=20):
@@ -112,69 +208,20 @@ def print_results(position_probabilities, average_points, qualification_probabil
 elo_ratings = fetch_elo_ratings()
 if elo_ratings is not None:
     teams = elo_ratings
-    initial_points = {
-        "Dinamo Zagreb": 39,
-        "Rijeka": 46,
-        "Hajduk": 45,
-        "Osijek": 30,
-        "Lok Zagreb": 28,
-        "Varazdin": 33,
-        "Istra 1961": 28,
-        "Slaven Belupo": 32,
-        "HNK Gorica": 24,
-        "Sibenik": 18
-    }
-else:
-    teams = {
-        "Dinamo Zagreb": 1527,
-        "Rijeka": 1473,
-        "Hajduk": 1468,
-        "Osijek": 1385,
-        "Lok Zagreb": 1359,
-        "Varazdin": 1321,
-        "Istra 1961": 1312,
-        "Slaven Belupo": 1303,
-        "HNK Gorica": 1268,
-        "Sibenik": 1187
-    }
-    initial_points = {
-        "Dinamo Zagreb": 39,
-        "Rijeka": 46,
-        "Hajduk": 45,
-        "Osijek": 30,
-        "Lok Zagreb": 28,
-        "Varazdin": 33,
-        "Istra 1961": 28,
-        "Slaven Belupo": 32,
-        "HNK Gorica": 24,
-        "Sibenik": 18
-    }
+    
 
-fixtures = [
-    ("Rijeka", "Sibenik"), ("Varazdin", "Istra 1961"), ("Lok Zagreb", "Osijek"),
-    ("Hajduk", "HNK Gorica"), ("Slaven Belupo", "Dinamo Zagreb"), ("HNK Gorica", "Slaven Belupo"),
-    ("Sibenik", "Istra 1961"), ("Dinamo Zagreb", "Lok Zagreb"), ("Osijek", "Varazdin"),
-    ("Rijeka", "Hajduk"), ("Hajduk", "Sibenik"), ("Istra 1961", "Osijek"),
-    ("Lok Zagreb", "HNK Gorica"), ("Slaven Belupo", "Rijeka"), ("Varazdin", "Dinamo Zagreb"),
-    ("Istra 1961", "Dinamo Zagreb"), ("Lok Zagreb", "Rijeka"), ("Sibenik", "Osijek"),
-    ("Slaven Belupo", "Hajduk"), ("Varazdin", "HNK Gorica"), ("Dinamo Zagreb", "Osijek"),
-    ("HNK Gorica", "Istra 1961"), ("Hajduk", "Lok Zagreb"), ("Rijeka", "Varazdin"),
-    ("Slaven Belupo", "Sibenik"), ("Istra 1961", "Rijeka"), ("Lok Zagreb", "Slaven Belupo"),
-    ("Osijek", "HNK Gorica"), ("Sibenik", "Dinamo Zagreb"), ("Varazdin", "Hajduk"),
-    ("HNK Gorica", "Dinamo Zagreb"), ("Hajduk", "Istra 1961"), ("Lok Zagreb", "Sibenik"),
-    ("Rijeka", "Osijek"), ("Slaven Belupo", "Varazdin"), ("Dinamo Zagreb", "Rijeka"),
-    ("Istra 1961", "Slaven Belupo"), ("Osijek", "Hajduk"), ("Sibenik", "HNK Gorica"),
-    ("Varazdin", "Lok Zagreb"), ("Hajduk", "Dinamo Zagreb"), ("Lok Zagreb", "Istra 1961"),
-    ("Rijeka", "HNK Gorica"), ("Slaven Belupo", "Osijek"), ("Varazdin", "Sibenik"),
-    ("Dinamo Zagreb", "Slaven Belupo"), ("HNK Gorica", "Hajduk"), ("Istra 1961", "Varazdin"),
-    ("Osijek", "Lok Zagreb"), ("Sibenik", "Rijeka"), ("Hajduk", "Rijeka"),
-    ("Istra 1961", "Sibenik"), ("Lok Zagreb", "Dinamo Zagreb"), ("Slaven Belupo", "HNK Gorica"),
-    ("Varazdin", "Osijek"), ("Dinamo Zagreb", "Varazdin"), ("HNK Gorica", "Lok Zagreb"),
-    ("Osijek", "Istra 1961"), ("Rijeka", "Slaven Belupo"), ("Sibenik", "Hajduk")
-]
+tournament_id = 170
+season_id = 61243
+
+initial_points = get_standings(tournament_id, season_id)
+
+fixtures = get_fixtures(tournament_id, season_id)
 
 num_simulations = 10000
 k = 20
 position_probabilities, average_points, qualification_probabilities_top8, qualification_probabilities_top24 = run_multiple_simulations(teams, fixtures, initial_points, num_simulations, k=k)
 
 print_results(position_probabilities, average_points, qualification_probabilities_top8, qualification_probabilities_top24)
+
+
+
